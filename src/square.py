c1e05143ea821999dee48fec89309cc3bc6b51b3
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2022-01-18 01:41:14 +0300

    add square class

---
+++ src/square.py
@@ -0,0 +1,14 @@
+from base import Figure
+
+class Square(Figure):
+    def __init__(self, side):
+        self.side = side
+        super().__init__('square')
+    
+    @property
+    def area(self):
+        return self.side ** 2
+    
+    @property
+    def perimeter(self):
+        return self.side * 4
